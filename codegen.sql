f6ba26ee6c3807b28a08a3c5689619fe0e7c3d58
Joel mail <joeltrashmail23@gmail.com>
2025-11-11 16:37:18 +0000

    snapshots and more

---
+++ codegen.sql
@@ -0,0 +1,4 @@
+{{ codegen.generate_model_yaml(
+    model_names=['stg_sql_server_dbo__zipcodes', 'stg_sql_server_dbo__addresses'],
+    upstream_descriptions=True )
+}}
---
+++ models/staging/sql_server_dbo/base/base_sql_server_dbo__addresses.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('sql_server_dbo', 'addresses') }}
+
+),
+
+renamed_and_recasted as (
+
+    select
+        address_id::VARCHAR(50) AS address_id,
+        zipcode::NUMBER(38,0) AS zipcode,
+        country::VARCHAR(50) AS country,
+        address::VARCHAR(150) AS address,
+        state::VARCHAR(50) AS state,
+        CONVERT_TIMEZONE('UTC', _fivetran_synced) AS last_loaded_utc
+    from source
+    where _fivetran_deleted != 1
+)
+
+select * from renamed_and_recasted
